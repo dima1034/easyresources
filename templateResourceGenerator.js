ffabe9ba61b904886aabb9dafdb414f8d8ddac9f
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-06-25 16:12:45 +0300

    Change resources file extension to json

--- templateResourceGenerator.js
+++ templateResourceGenerator.js
@@ -26,7 +26,7 @@ var generator = function(){
 		function createResourceFiles(json) {
 			for (var locale in json) {
 				var jsonData = JSON.stringify(json[locale]);
-				var localeFileName = generateLocaleName(locale) + ".js";
+				var localeFileName = generateLocaleName(locale) + ".json";
 				fs.writeFileSync(outputDir + localeFileName, jsonData);
 				console.log(outputDir + localeFileName);
 			}
