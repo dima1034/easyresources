cf6165597fef9f55fa9b4e2205f06d9d3a41c52e
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-09-21 09:14:24 +0300

    Add app localization

--- README.md
+++ README.md
@@ -10,14 +10,20 @@
 
   $ easyresources --help
 
-  **Generate from excel workbook**
-  
-  $ easyresources *generate-from-excel-workbook* -t "Simple course" -f "resources.xlsx" -o "_output/"
-  
-  $ easyresources *ge* -t "Simple course"
+  **Generate application resources**
 
-  **Generate from google spreadsheet**
+  $ easyresources *generate-app-resources* -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
 
-  $ easyresources *generate-from-google-spreadsheet* -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output/"
+  $ easyresources *ga*
+
+  **Generate template resources**
+
+  $ easyresources *generate-template-resources* -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
   
-  $ easyresources *generate* -t "Simple course"
+  $ easyresources *gt* -t "Simple course"
+
+  **Generate template resources from local excel workbook**
+  
+  $ easyresources *generate-template-resources-from-local-file* -t "Simple course" -f "resources.xlsx" -o "_output"
+  
+  $ easyresources *gtf* -t "Simple course"
--- index.js
+++ index.js
@@ -4,37 +4,51 @@ var program = require('commander'),
     resourceGenerationService = require('./lib/resourceGenerationService');
 
 program
-    .version('0.0.6')
+    .version('0.0.8')
     .on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('    $ easyresources generate-from-google-spreadsheet -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output/"');
-    console.log('    $ easyresources generate -t "Simple course"');
+    console.log('    $ easyresources generate-app-resources -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
+    console.log('    $ easyresources ga');
     console.log('');
-    console.log('    $ easyresources generate-from-excel-workbook -t "Simple course" -f "resources.xlsx" -o "_output/"');
-    console.log('    $ easyresources ge -t "Simple course"');
+    console.log('    $ easyresources generate-template-resources -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
+    console.log('    $ easyresources gt -t "Simple course"');
+    console.log('');
+    console.log('    $ easyresources generate-template-resources-from-local-file -t "Simple course" -f "resources.xlsx" -o "_output"');
+    console.log('    $ easyresources gtf -t "Simple course"');
 });
 
 program
-  .command('generate-from-google-spreadsheet')
-  .alias('generate')
+  .command('generate-app-resources')
+  .alias('ga')
+  .description('Generates json resource files for app from google spreadsheet resource file')
+  .option("-w, --worksheetName [templateName]", "Worksheet name [Resources]")
+  .option("-f, --resourceId [resourceFileId]", "Google spreadsheet resource file id [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
+  .option("-o, --outputPath [outputPath]", "Output path [_output]")
+  .action(function (options) {
+    resourceGenerationService.generateAppResources(options.templateName, options.resourceFileId, options.outputPath);
+});
+
+program
+  .command('generate-template-resources')
+  .alias('gt')
   .description('Generates json resource files for templates from google spreadsheet resource file')
   .option("-t, --templateName <templateName>", "Template name")
-  .option("-f, --resourceId [resourceFileId]", "Google spreadsheet resource file id")
-  .option("-o, --outputPath [outputPath]", "Output path")
+  .option("-f, --resourceId [resourceFileId]", "Google spreadsheet resource file id [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
+  .option("-o, --outputPath [outputPath]", "Output path [_output]")
   .action(function (options) {
-    resourceGenerationService.generateFromGoogleSpreadsheet(options.templateName, options.resourceFileId, options.outputPath);
+    resourceGenerationService.generateTemplateResources(options.templateName, options.resourceFileId, options.outputPath);
 });
 
 program
-  .command('generate-from-excel-workbook')
-  .alias('ge')
+  .command('generate-template-resources-from-local-file')
+  .alias('gtf')
   .description('Generates json resource files for templates from excel workbook(*.xlsx) resource file')
   .option("-t, --templateName <templateName>", "Template name")
-  .option("-f, --resourceFilePath [resourceFilePath]", "Resource file path *.xlsx")
-  .option("-o, --outputPath [outputPath]", "Output path")
+  .option("-f, --resourceFilePath [resourceFilePath]", "Resource file path *.xlsx [resources.xlsx]")
+  .option("-o, --outputPath [outputPath]", "Output path [_output]")
   .action(function (options) {
-    resourceGenerationService.generateFromExcelWorkbook(options.templateName, options.resourceFilePath, options.outputPath);
+    resourceGenerationService.generateTemplateResourcesFromLocalFile(options.templateName, options.resourceFilePath, options.outputPath);
 });
 
 program.parse(process.argv);
