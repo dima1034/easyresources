bc8221143b001e7402fd8177134660b94c689a71
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-09-22 11:17:07 +0300

    Merge all comands into one - generate command

--- README.md
+++ README.md
@@ -12,18 +12,33 @@
 
   **Generate application resources**
 
-  $ easyresources *generate-app-resources* -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
+  $ easyresources *g*
 
-  $ easyresources *ga*
+  $ easyresources *generate* -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
+
+  $ easyresources *generate* --worksheetName "Resources" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"
+
 
   **Generate template resources**
 
-  $ easyresources *generate-template-resources* -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
-  
-  $ easyresources *gt* -t "Simple course"
+  $ easyresources *g template* -w "Simple course"'
 
-  **Generate template resources from local excel workbook**
-  
-  $ easyresources *generate-template-resources-from-local-file* -t "Simple course" -f "resources.xlsx" -o "_output"
-  
-  $ easyresources *gtf* -t "Simple course"
+  $ easyresources *generate template* -w "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"'
+
+  $ easyresources *generate template* --worksheetName "Simple course" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"'
+
+  **Generate app resources from local .xlsx file**
+
+  $ easyresources *g* -f "resources.xlsx"'
+
+  $ easyresources *generate* -w "Resources" -f "resources.xlsx" -o "_output"'
+
+  $ easyresources *generate* --worksheetName "Resources" --resourceFile "resources.xlsx" --outputPath "_output"'
+    
+  **Generate template resources from local .xlsx file**
+
+  $ easyresources *g template* -w "Simple course" -f "resources.xlsx"'
+
+  $ easyresources *generate template* -w "Simple course" -f "resources.xlsx" -o "_output"'
+
+  $ easyresources *generate template* --worksheetName "Simple course" --resourceFile "resources.xlsx" --outputPath "_output"'
--- index.js
+++ index.js
@@ -8,47 +8,40 @@ program
     .on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('    $ easyresources generate-app-resources -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
-    console.log('    $ easyresources ga');
+    console.log('  Generate app resources');
     console.log('');
-    console.log('    $ easyresources generate-template-resources -t "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
-    console.log('    $ easyresources gt -t "Simple course"');
+    console.log('    $ easyresources g');
+    console.log('    $ easyresources generate -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
+    console.log('    $ easyresources generate --worksheetName "Resources" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"');
     console.log('');
-    console.log('    $ easyresources generate-template-resources-from-local-file -t "Simple course" -f "resources.xlsx" -o "_output"');
-    console.log('    $ easyresources gtf -t "Simple course"');
+    console.log('  Generate template resources');
+    console.log('');
+    console.log('    $ easyresources g template -w "Simple course"');
+    console.log('    $ easyresources generate template -w "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
+    console.log('    $ easyresources generate template --worksheetName "Simple course" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"');
+    console.log('');
+    console.log('  Generate app resources from local .xlsx file');
+    console.log('');
+    console.log('    $ easyresources g -f "resources.xlsx"');
+    console.log('    $ easyresources generate -w "Resources" -f "resources.xlsx" -o "_output"');
+    console.log('    $ easyresources generate --worksheetName "Resources" --resourceFile "resources.xlsx" --outputPath "_output"');
+    console.log('');
+    console.log('  Generate template resources from local .xlsx file');
+    console.log('');
+    console.log('    $ easyresources g template -w "Simple course" -f "resources.xlsx"');
+    console.log('    $ easyresources generate template -w "Simple course" -f "resources.xlsx" -o "_output"');
+    console.log('    $ easyresources generate template --worksheetName "Simple course" --resourceFile "resources.xlsx" --outputPath "_output"');
 });
 
 program
-  .command('generate-app-resources')
-  .alias('ga')
-  .description('Generates json resource files for app from google spreadsheet resource file')
-  .option("-w, --worksheetName [templateName]", "Worksheet name [Resources]")
-  .option("-f, --resourceId [resourceFileId]", "Google spreadsheet resource file id [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
+  .command('generate [mode]')
+  .alias('g')
+  .description('Generates json resource files spreadsheet resource file')
+  .option("-w, --worksheetName [worksheetName]", "Worksheet name [Resources]")
+  .option("-f, --resourceFile [resourceFile]", "Spreadsheet resource file [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
   .option("-o, --outputPath [outputPath]", "Output path [_output]")
-  .action(function (options) {
-    resourceGenerationService.generateAppResources(options.templateName, options.resourceFileId, options.outputPath);
-});
-
-program
-  .command('generate-template-resources')
-  .alias('gt')
-  .description('Generates json resource files for templates from google spreadsheet resource file')
-  .option("-t, --templateName <templateName>", "Template name")
-  .option("-f, --resourceId [resourceFileId]", "Google spreadsheet resource file id [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
-  .option("-o, --outputPath [outputPath]", "Output path [_output]")
-  .action(function (options) {
-    resourceGenerationService.generateTemplateResources(options.templateName, options.resourceFileId, options.outputPath);
-});
-
-program
-  .command('generate-template-resources-from-local-file')
-  .alias('gtf')
-  .description('Generates json resource files for templates from excel workbook(*.xlsx) resource file')
-  .option("-t, --templateName <templateName>", "Template name")
-  .option("-f, --resourceFilePath [resourceFilePath]", "Resource file path *.xlsx [resources.xlsx]")
-  .option("-o, --outputPath [outputPath]", "Output path [_output]")
-  .action(function (options) {
-    resourceGenerationService.generateTemplateResourcesFromLocalFile(options.templateName, options.resourceFilePath, options.outputPath);
+  .action(function (mode, options) {
+    resourceGenerationService.generateResources(mode, options.worksheetName, options.resourceFile, options.outputPath);
 });
 
 program.parse(process.argv);
