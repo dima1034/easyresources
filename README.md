2d15bee3f27cee564073e76c7a9987729e454a05
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-09-24 15:21:53 +0300

    Refactor data receiving

--- README.md
+++ README.md
@@ -1,44 +1,33 @@
 ﻿# easyresources
 
-  Console app to easily generate `json` resource files from `xlsx` or `google spreadsheet` resource file 
+  Console app to easily generate *json* resource files from *xlsx* or *google spreadsheet* resource file 
 
 ## Installation
 
   npm install easyresources
 
-## Usage
+### Usage
+
+  **Generate resources**
+
+   $ easyresources g *%MODE%*
+
+   $ easyresources generate *%MODE%* -w "Resources" -f "*%RESOURCE_FILE%*" -o "_output"
+
+   $ easyresources generate *%MODE%* --worksheetName "Resources" --resourceFile "*%RESOURCE_FILE%*" --outputPath "_output"
+
+   *%MODE%* - optional. Possible values:
+
+       • app - is used by default;
+       • template
+
+   *%RESOURCE_FILE%* possible values:
+
+       • Google spreadsheet file id, eg "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58";
+       • Local .xlsx file, eg "resources.xlsx"
+
+  **Help**
+
+  $ easyresources -h
 
   $ easyresources --help
-
-  **Generate application resources**
-
-  $ easyresources *g*
-
-  $ easyresources *generate* -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"
-
-  $ easyresources *generate* --worksheetName "Resources" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"
-
-
-  **Generate template resources**
-
-  $ easyresources *g template* -w "Simple course"'
-
-  $ easyresources *generate template* -w "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"'
-
-  $ easyresources *generate template* --worksheetName "Simple course" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"'
-
-  **Generate app resources from local .xlsx file**
-
-  $ easyresources *g* -f "resources.xlsx"'
-
-  $ easyresources *generate* -w "Resources" -f "resources.xlsx" -o "_output"'
-
-  $ easyresources *generate* --worksheetName "Resources" --resourceFile "resources.xlsx" --outputPath "_output"'
-    
-  **Generate template resources from local .xlsx file**
-
-  $ easyresources *g template* -w "Simple course" -f "resources.xlsx"'
-
-  $ easyresources *generate template* -w "Simple course" -f "resources.xlsx" -o "_output"'
-
-  $ easyresources *generate template* --worksheetName "Simple course" --resourceFile "resources.xlsx" --outputPath "_output"'
--- index.js
+++ index.js
@@ -4,33 +4,25 @@ var program = require('commander'),
     resourceGenerationService = require('./lib/resourceGenerationService');
 
 program
-    .version('0.0.11')
+    .version('0.0.12')
     .on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('  Generate app resources');
+    console.log('  Generate resources');
     console.log('');
-    console.log('    $ easyresources g');
-    console.log('    $ easyresources generate -w "Resources" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
-    console.log('    $ easyresources generate --worksheetName "Resources" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"');
+    console.log('    $ easyresources g %MODE%');
+    console.log('    $ easyresources generate %MODE% -w "Resources" -f "%RESOURCE_FILE%" -o "_output"');
+    console.log('    $ easyresources generate %MODE% --worksheetName "Resources" --resourceFile "%RESOURCE_FILE%" --outputPath "_output"');
     console.log('');
-    console.log('  Generate template resources');
+    console.log('  %MODE% - optional. Possible values:');
     console.log('');
-    console.log('    $ easyresources g template -w "Simple course"');
-    console.log('    $ easyresources generate template -w "Simple course" -f "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" -o "_output"');
-    console.log('    $ easyresources generate template --worksheetName "Simple course" --resourceFile "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58" --outputPath "_output"');
+    console.log('    • app - is used by default');
+    console.log('    • template');
     console.log('');
-    console.log('  Generate app resources from local .xlsx file');
+    console.log('  %RESOURCE_FILE% possible values:');
     console.log('');
-    console.log('    $ easyresources g -f "resources.xlsx"');
-    console.log('    $ easyresources generate -w "Resources" -f "resources.xlsx" -o "_output"');
-    console.log('    $ easyresources generate --worksheetName "Resources" --resourceFile "resources.xlsx" --outputPath "_output"');
-    console.log('');
-    console.log('  Generate template resources from local .xlsx file');
-    console.log('');
-    console.log('    $ easyresources g template -w "Simple course" -f "resources.xlsx"');
-    console.log('    $ easyresources generate template -w "Simple course" -f "resources.xlsx" -o "_output"');
-    console.log('    $ easyresources generate template --worksheetName "Simple course" --resourceFile "resources.xlsx" --outputPath "_output"');
+    console.log('    • Google spreadsheet file id, eg "1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58"');
+    console.log('    • Local .xlsx file, eg "resources.xlsx"');
 });
 
 program
