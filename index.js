c17584a72db887996220895b9ac8ffba1d664cbe
YuliaSavchuk <yulia.savchuk3@gmail.com>
2016-08-22 14:43:26 +0300

    Update tool to generate resources for froala

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ var program = require('commander'),
     resourceGenerationService = require('./lib/resourceGenerationService');
 
 program
-    .version('0.0.16')
+    .version('0.0.17')
     .on('--help', function () {
     console.log('  Examples:');
     console.log('');
     console.log('  Generate resources');
     console.log('');
-    console.log('    $ easyresources g %MODE%');
-    console.log('    $ easyresources generate %MODE% -w "Resources" -f "%RESOURCE_FILE%" -o "_output" -e "nl, de"');
-    console.log('    $ easyresources generate %MODE% --worksheetName "Resources" --resourceFile "%RESOURCE_FILE%" --outputPath "_output" --excludeLocalizations "nl, de"');
+    console.log('    $ easyresources g');
+    console.log('    $ easyresources g -w "Resources" -f "%RESOURCE_FILE%" -o "_output" -d 1 -e "nl, de"');
+    console.log('    $ easyresources generate %MODE% --worksheetName "Resources" --resourceFile "%RESOURCE_FILE%" --outputPath "_output" --useDefaultLocalizationWhenNoTranslation 1 --excludeLocalizations "nl, de"');
     console.log('');
-    console.log('  %MODE% - optional. Possible values:');
-    console.log('');
-    console.log('    • app - is used by default');
-    console.log('    • template');
     console.log('');
     console.log('  %RESOURCE_FILE% possible values:');
     console.log('');
@@ -26,15 +22,16 @@ program
 });
 
 program
-  .command('generate [mode]')
+  .command('generate')
   .alias('g')
   .description('Generates json resource files spreadsheet resource file')
   .option("-w, --worksheetName [worksheetName]", "Worksheet name [Resources]")
   .option("-f, --resourceFile [resourceFile]", "Spreadsheet resource file [1TtXjyDSHvPPPUVo9dynM-HyeuA9wT2O852stPKLqV58]")
   .option("-o, --outputPath [outputPath]", "Output path [_output]")
   .option("-e, --excludeLocalizations [excludeLocalization]", "Exclude localizations, separated by ','")
-  .action(function (mode, options) {
-    resourceGenerationService.generateResources(mode, options);
+  .option("-d, --useDefaultLocalizationWhenNoTranslation [useDefaultLocalizationWhenNoTranslation]", "Use default localization when no translation")
+  .action(function (options) {
+    resourceGenerationService.generateResources(options);
 });
 
 program.parse(process.argv);
